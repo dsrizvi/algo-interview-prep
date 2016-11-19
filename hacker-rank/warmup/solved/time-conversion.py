2572daee6c80607f0f85fd2bf00e82bc122628a7
Danyal Rizvi <d.s.rizvi@gmail.com>
2016-11-18 23:24:00 -0600

    angry professor solved

--- hacker-rank/warmup/solved/time-conversion.py
+++ hacker-rank/warmup/solved/time-conversion.py
@@ -4,17 +4,17 @@ import sys
 
 def convert_time(time):
     period = time[-2:]
-    hours = time[0:1]
+    hours = time[0:2]
     converted_hours = ''
 
     if period == 'AM':
-        converted_hours = str(int(hours) % 12)
+        converted_hours = int(hours) % 12
     if period == 'PM':
         if hours != '12':
-            converted_hours = str(int(hours) + 12)
+            converted_hours = int(hours) + 12
         else:
             converted_hours = hours
-
+    converted_hours = str(converted_hours) if converted_hours > 9 else ('0' + str(converted_hours))
     return (converted_hours + time[2:-2])
 
 
